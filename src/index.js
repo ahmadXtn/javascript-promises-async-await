9428148019fac097de5bcd0d01151732cb81b6ef
ahmad <Ahmed.Bouazizi@issatmh.u-monastir.tn>
2020-10-14 10:59:10 +0100

    Fetching Movies with a Traditional Promise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import {fetchWithTimeout} from "./services";
 
-
+const movies=  require('movies');
 export function fetchMovies() {
-	
+	const resolveFunction = () => movies;
+	return fetchWithTimeout(1000).then(resolveFunction)
 }
+
+
+
+
+let moviePromise=fetchMovies()
+	.then(result=>console.log(result))
